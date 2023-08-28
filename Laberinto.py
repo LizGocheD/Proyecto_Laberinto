4b1f086dfc46ffd6f583e7c015e8032a1fc29ab3
LizGoche <liz.goche.d@gmail.com>
2023-08-28 11:57:22 -0500

    se agrega nueva modificación = version2

--- Laberinto.py
+++ Laberinto.py
@@ -15,6 +15,14 @@ laberinto = [
 # Coordenadas del personaje
 posicion_personaje = (1, 1)
 
+# Pedir el nombre del jugador por teclado
+nombre_jugador = input("Ingrese su nombre: ")
+
+# Imprimir mensaje de bienvenida con el nombre
+print(f"Bienvenido/a, {nombre_jugador}!Hora de jugar")
+
+print("Mueve al personaje con las teclas direccionales o presiona 'q' para salir:")
+
 # Imprimir laberinto
 def imprimir_laberinto():
     for fila in laberinto:
@@ -29,36 +37,39 @@ def mover_personaje(direccion):
     if direccion == "↑":
         nueva_fila = fila - 1
         if laberinto[nueva_fila][columna] != "#":
+            laberinto[fila] = laberinto[fila][:columna] + "." + laberinto[fila][columna+1:]
+            laberinto[nueva_fila] = laberinto[nueva_fila][:columna] + "P" + laberinto[nueva_fila][columna+1:]
             posicion_personaje = (nueva_fila, columna)
     elif direccion == "↓":
         nueva_fila = fila + 1
         if laberinto[nueva_fila][columna] != "#":
+            laberinto[fila] = laberinto[fila][:columna] + "." + laberinto[fila][columna+1:]
+            laberinto[nueva_fila] = laberinto[nueva_fila][:columna] + "P" + laberinto[nueva_fila][columna+1:]
             posicion_personaje = (nueva_fila, columna)
     elif direccion == "←":
         nueva_columna = columna - 1
         if laberinto[fila][nueva_columna] != "#":
+            laberinto[fila] = laberinto[fila][:columna] + "." + laberinto[fila][columna+1:]
+            laberinto[fila] = laberinto[fila][:nueva_columna] + "P" + laberinto[fila][nueva_columna+1:]
             posicion_personaje = (fila, nueva_columna)
     elif direccion == "→":
         nueva_columna = columna + 1
         if laberinto[fila][nueva_columna] != "#":
+            laberinto[fila] = laberinto[fila][:columna] + "." + laberinto[fila][columna+1:]
+            laberinto[fila] = laberinto[fila][:nueva_columna] + "P" + laberinto[fila][nueva_columna+1:]
             posicion_personaje = (fila, nueva_columna)
 
-# Pedir el nombre del jugador por teclado
-nombre_jugador = input("Ingrese su nombre: ")
-
-# Imprimir mensaje de bienvenida con el nombre
-print(f"Bienvenido/a, {nombre_jugador}!Hora de jugar")
 
 # Ciclo del juego
 while True:
     imprimir_laberinto()
-    print("Mueve al personaje con las teclas direccionales o presiona 'q' para salir:")
     direccion = input()
+
+    mover_personaje(direccion)
 
     if direccion == "q":
         break
 
-    mover_personaje(direccion)
 
     # Limpiar la pantalla
     import os
